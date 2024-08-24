54a7fbae41291ff8b0968bcc28c358c80532969f
Chonakan Chumtap <cshuhand@gmail.com>
2024-08-24 11:45:40 +0700

    :memo: : fix chess02.

--- assignment07/chess02.py
+++ assignment07/chess02.py
@@ -1,18 +1,27 @@
 import asyncio
 import time
 
-my_compute_time = 0.05
-opponent_compute_time = 0.55
-opponents = 24
+my_compute_time = 0.1
+opponent_compute_time = 0.5
+opponents = 6
 move_pairs = 30
+
+# 1 = 18 sec
+# 2 = 18 sec
+# 3 = 18 sec
+# 4 = 18 sec
+# 5 = 18 sec
+# 6 = 19 sec
+# 10 = 32 sec
+# 11 = 35 sec
+# 12 = 38 sec
+# 15 = 47 sec
+# 24 = 75 sec
 
 # Again notice that I declare the main() function as a async function
 async def main(x):
     start_counter = time.perf_counter()
     for i in range (move_pairs):
-        # print(f"Board-{x} {i+1} Judit thinking of making a move")
-        # Don't use time.sleep in a async function. I'm using it because in reality you aren't thinking about
-        # move on 24 boards at the same time, and so I need to block the event loop
         time.sleep(my_compute_time)
         print(f"Board - {x+1} {i+1} Judit made a move")
         # Here our opponent is making their turn and now we can move onto the next board.
