0bbdd26ea12e8cd343f71c5508520fe9d26e167b
Chonakan Chumtap <cshuhand@gmail.com>
2024-09-24 21:03:30 +0700

    :sparkles: : done kafka.

--- bonus/producer.py
+++ bonus/producer.py
@@ -1,1 +1,18 @@
-# https://aiokafka.readthedocs.io/en/stable/
+from aiokafka import AIOKafkaProducer
+import asyncio
+
+async def send_one():
+    producer = AIOKafkaProducer(
+        bootstrap_servers='192.168.137.1:9092')
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    try:
+        # Produce message
+        print("Sending...")
+        await producer.send_and_wait("my_topic", b"Hello from Karn")
+    finally:
+        # Wait for all pending messages to be delivered or expire.
+        print("Finally...")
+        await producer.stop()
+
+asyncio.run(send_one())
