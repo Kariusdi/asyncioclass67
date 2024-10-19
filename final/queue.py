df3d40bd9c44e61b4846ce5d19fa0e850a22ae71
Chonakan Chumtap <cshuhand@gmail.com>
2024-10-20 00:26:31 +0700

    :zap: : locate new timer.

--- final/queue.py
+++ final/queue.py
@@ -3,33 +3,30 @@ import asyncio
 
 async def producer(queue, id):
     counter = 0
-    start_time = time.perf_counter()
+    timer = time.perf_counter()
     while counter <= 2:
         await queue.put(f"{id}-{counter}")
-        print(f"Time: {round(time.perf_counter() - start_time, 2)} - Producer {id} produced-> Item: {id}-{counter}")
+        print("Time: {:.2f} - Producer {} produced-> Item: {}-{}".format(time.perf_counter() - timer, id, id, counter))
         counter += 1
         await asyncio.sleep(1)
     await queue.put(None)
 
 async def consumer(queue):
-    start_time = time.perf_counter()
+    timer = time.perf_counter()
     while True:
         item = await queue.get()
         if item is None:
-            print(f'Time: {round(time.perf_counter() - start_time, 2)} - Consumer consumed None')
+            print('Time: {:.2f} - Consumer consumed None'.format(time.perf_counter() - timer, item))
             break
-        print(f'Time: {round(time.perf_counter() - start_time, 2)} - Consumer consumed Item: {item}')    
+        print('Time: {:.2f} - Consumer consumed Item: {}'.format(time.perf_counter() - timer, item))    
 
 async def main():
     queue = asyncio.Queue()
-    start_time = time.perf_counter()
-    
     producer_tasks = [asyncio.create_task(producer(queue, id)) for id in ["A", "B", "C"]]
     consumer_task = asyncio.create_task(consumer(queue))
-
     await asyncio.gather(*producer_tasks, consumer_task)
-    
-    print(f"Time: {round(time.perf_counter() - start_time, 2)} sec")
 
 if __name__ == "__main__":
+    start_time = time.perf_counter()
     asyncio.run(main())
+    print("Time: {:.2f} sec".format(time.perf_counter() - start_time))
